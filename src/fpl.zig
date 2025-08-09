9f004a24d60298d80b887f7932d4fa5e54437dcc
xydone <xydone@outlook.com>
2025-08-09 15:09:14 +0300

    fix: resolve runtime panic when data folder not present, remove reference vaxis code

--- src/fpl.zig
+++ src/fpl.zig
@@ -165,9 +165,21 @@ pub const GetFixtures = struct {
         return request;
     }
 };
-
 fn saveToFile(allocator: Allocator, T: type, response: T, path: []const u8) !void {
-    const file = try std.fs.cwd().createFile(path, .{});
+    const file = std.fs.cwd().createFile(path, .{}) catch |err| file: {
+        switch (err) {
+            // if the parent folder is not found, FileNotFound is thrown.
+            error.FileNotFound => {
+                // fs.path.dirname() returns null if the path is the root dir
+                const dirname = std.fs.path.dirname(path) orelse return err;
+                // create the dir
+                try std.fs.cwd().makeDir(dirname);
+                // retry creating the file
+                break :file try std.fs.cwd().createFile(path, .{});
+            },
+            else => return err,
+        }
+    };
 
     const body_string = try std.json.stringifyAlloc(allocator, response, .{});
     defer allocator.free(body_string);
